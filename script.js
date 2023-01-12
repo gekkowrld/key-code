1c622ed11a56579a8fb9a694d1e9e8bfe52b0447
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-01-12 19:45:00 +0300

    Different approach which is much better

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-function keyPress(e){
-    const main = document.querySelector('main')
-    const keyNum =document.createElement('p');
+window.addEventListener('keydown', keyDown);
+function keyDown(e){
+    let charKey = e.key;
+    let theKeyNum = charKey.charCodeAt();
+    const main = document.querySelector('main');
+    const div = document.createElement('div');
     const keyName = document.createElement('p');
-    const keyBox = document.createElement('div');
-    keyNum.setAttribute('class','key-num')
-    keyName.setAttribute('class','key-name');
-    main.appendChild(keyBox);
-    keyBox.appendChild(keyName);
-    keyBox.appendChild(keyNum)
-    keyNum.innerText = e.which || e.keyCode;
-    if(e.key === undefined || e.keyCode === undefined){
-        keyName.innerText = ':(';
-        keyNum.innerText = ':('
-    }else if(e.key === ' ') keyName.innerText = 'SpaceBar';
-    else keyName.innerHTML = e.key;
-    const winHeight = main.clientHeight;
-    const winWidth = main.clientWidth;
-    scrollTo(winWidth, winHeight);
-    console.log(e.key);
+    const keyNum = document.createElement('p');
+    main.appendChild(div);
+    div.appendChild(keyName);
+    div.appendChild(keyNum);
+    keyName.setAttribute('class', 'key-name');
+    keyNum.setAttribute('class','key-num');
+    if(charKey === undefined){
+        keyName.innerText = ':('
+        keyNum.innerText = ':(';
+    }else if(charKey === " "){
+        keyName.innerText = 'SpaceBar';
+        keyNum.innerText = theKeyNum;
+    }else{
+        keyName.innerText = charKey;
+        keyNum.innerText = theKeyNum;
+    }
+    let y = main.clientHeight;
+    let x = main.clientWidth;
+    scrollTo(x,y)
 }
-window.addEventListener('keyup', keyPress);
+
+const mode = document.querySelector('.mode')
+mode.addEventListener('click', modeSwitch);
+function modeSwitch(){
+    let darkBody = document.querySelector('body');
+    const darkMd = Array.from(document.querySelectorAll('p'));
+    darkMd.forEach(p => p.classList.toggle('dark'))
+    darkBody.classList.toggle('dark');
+    if(mode.innerText === 'Dark Mode'){
+        mode.innerText = 'Light Mode';
+    }else{
+        mode.innerText = 'Dark Mode'
+    }
+}
